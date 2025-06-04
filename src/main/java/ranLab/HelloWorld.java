83ff7872259f0fac4f6e8f7d4d93b7e3927e90bf
ax2d-ani <147320083+ax2d-ani@users.noreply.github.com>
2025-06-04 13:58:49 +0530

    Update HelloWorld.java

--- src/main/java/ranLab/HelloWorld.java
+++ src/main/java/ranLab/HelloWorld.java
@@ -1,9 +1,19 @@
 package ranLab;
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 'a' value: ");
+		int a=sc.nextInt();
+		System.out.println("The factorial of "+a + " is: "+factorial(a));
 	}
-
+	
+	public static int factorial(int n) {
+		if(n==1)
+			return 1;
+		else
+			return n*factorial(n-1);
+	}
 }
